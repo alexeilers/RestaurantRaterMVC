5f49ba641be444cad173e0914c7d3f634448b469
Eilers Fam <alexander.eilers@gmail.com>
2022-04-12 16:39:59 -0400

    added restaurant details and edit functionality

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -31,6 +31,27 @@ namespace RestaurantRaterMVC.Controllers
                 return View(restaurants);
         }
 
+        [ActionName("Details")]
+        public async Task<IActionResult> Restaurant(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants
+                .Include(r => r.Ratings)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
+            if (restaurant == null)
+                return RedirectToAction(nameof(Index));
+
+            RestaurantDetail restaurantDetail = new RestaurantDetail()
+            {
+                Id = restaurant.Id,
+                Name = restaurant.Name,
+                Location = restaurant.Location,
+                Score = restaurant.Score,
+            };
+
+            return View(restaurantDetail);
+        }
+
         public async Task<IActionResult> Create()
         {
             return View();
@@ -53,7 +74,43 @@ namespace RestaurantRaterMVC.Controllers
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
+        }
 
+        public async Task<IActionResult> Edit(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant == null)
+                return RedirectToAction(nameof(Index));
+
+            RestaurantEdit restaurantEdit = new RestaurantEdit ()
+            {
+                Id = restaurant.Id,
+                Name = restaurant.Name,
+                Location = restaurant.Location,
+            };
+
+            return View(restaurantEdit);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, RestaurantEdit model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ModelState);
+            }
+
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant == null)
+                return RedirectToAction(nameof(Index));
+            
+            restaurant.Name = model.Name;
+            restaurant.Location = model.Location;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Details", new { id = restaurant.Id });
         }
     }
 }
