b7e002fb056f6333064420302a74a86354111612
Eilers Fam <alexander.eilers@gmail.com>
2022-04-12 21:17:30 -0400

    added rating create functionality

---
+++ Controllers/RatingController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RestaurantRaterMVC.Data;
+using RestaurantRaterMVC.Models.Rating;
+
+namespace RestaurantRaterMVC.Controllers
+{
+    public class RatingController : Controller
+    {
+        private RestaurantDbContext _context;
+        
+        public RatingController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<RatingListItem> ratings = await _context.Ratings
+                .Select(r => new RatingListItem()
+                {
+                    RestaurantName = r.Restaurant.Name,
+                    Score = r.Score,
+                }).ToListAsync();
+
+                return View(ratings);
+        }
+
+        public async Task<IActionResult> Restaurant(int id)
+        {
+            IEnumerable<RatingListItem> ratings = await _context.Ratings
+                .Where(r => r.RestaurantId == id)
+                .Select(r => new RatingListItem()
+                {
+                    RestaurantName = r.Restaurant.Name,
+                    Score = r.Score,
+                }).ToListAsync();
+
+                Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+                ViewBag.RestaurantName = restaurant.Name;
+
+                return View(ratings);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            IEnumerable<SelectListItem> restaurantOptions = await _context.Restaurants.Select(r => new SelectListItem()
+            {
+                Text = r.Name,
+                Value = r.Id.ToString()
+            }).ToListAsync();
+
+            RatingCreate model = new RatingCreate();
+            model.RestaurantOptions = restaurantOptions;
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RatingCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Rating rating = new Rating()
+            {
+                RestaurantId = model.RestaurantId,
+                Score = model.Score,
+            };
+
+            _context.Ratings.Add(rating);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
